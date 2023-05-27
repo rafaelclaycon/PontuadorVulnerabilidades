e4a40e4e392ae23169bfbf4cee4a1ce22b316e66
Rafael Schmitt <rafael.claycon@gmail.com>
2023-05-27 16:05:42 -0300

    Initial Commit

---
+++ PontuadorVulnerabilidades/PontuadorVulnerabilidadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PontuadorVulnerabilidadesApp.swift
+//  PontuadorVulnerabilidades
+//
+//  Created by Rafael Schmitt on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PontuadorVulnerabilidadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
